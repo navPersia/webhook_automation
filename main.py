f443e1da4d4fcefe1fbe058b2ad920adefeb19ec
Navid-Radkusha <navidradkusha@gmail.com>
2022-06-11 19:48:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, Response
-import subprocess
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['POST'])
 def hello():
-    subprocess.call("sudo bash ./automation.sh", shell=True)
-    return "worked"
-
-
+    os.system("sudo docker-compose -f /home/pi/docker/docker-compose.yml pull")
+    os.system("sudo docker-compose -f /home/pi/docker/docker-compose.yml up --detach")
+    os.system("sudo docker image prune -f")
+    return Response(status=200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5009)
